1f8d0dd193ba77b0788f8b7563b2c5d40f06bab9
SABARI RAJA PERIYASAMY <sabarirajaperiyasamy@sabaris-macbook-pro.local>
2016-04-14 14:54:39 +0530

    Hello world new change in UI

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -19,7 +19,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func goButtonPress(sender: UIButton) {
+       var abc = "hello"
+        if(!textMsg.hidden){
+             abc = textMsg.text!
+            mGreet.text = "you  are welcome to swift  ! Dear " + abc
+            textMsg.hidden = true
+        }else{
+            textMsg.hidden = false
+            textMsg.text=""
+            mGreet.text="your request Dear"
+        }
+        
+
+       
+        
+    }
 
 
+    @IBOutlet weak var gBbuttonPressed: UIButton!
+    @IBOutlet weak var mGreet: UILabel!
+    @IBOutlet weak var textMsg: UITextField!
 }
 
